3395ada33ccb05542e501ec2bba0745b636633d8
Alessio Mora <alessio.mora@unibo.it>
2022-02-09 10:52:45 +0100

    Commented SSTC code

--- sstc/emnist_fedavg_main.py
+++ sstc/emnist_fedavg_main.py
@@ -35,8 +35,9 @@ flags.DEFINE_float('client_learning_rate', 0.1, 'Client learning rate.')
 
 FLAGS = flags.FLAGS
 
+
 def get_emnist_dataset():
-  """Loads and preprocesses the EMNIST dataset.
+    """Loads and preprocesses the EMNIST dataset.
 
   Returns:
     A `(emnist_train, emnist_test)` tuple where `emnist_train` is a
@@ -44,32 +45,31 @@ def get_emnist_dataset():
     and `emnist_test` is a single `tf.data.Dataset` representing the test data
     of all clients.
   """
-  emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data(
-      only_digits=False)
+    emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data(
+        only_digits=False)
 
-  def element_fn(element):
-    return collections.OrderedDict(
-        x=tf.expand_dims(element['pixels'], -1), y=element['label'])
+    def element_fn(element):
+        return collections.OrderedDict(
+            x=tf.expand_dims(element['pixels'], -1), y=element['label'])
 
-  def preprocess_train_dataset(dataset):
-    # Use buffer_size same as the maximum client dataset size,
-    # 418 for Federated EMNIST
-    return dataset.map(element_fn).shuffle(buffer_size=418).repeat(
-        count=FLAGS.client_epochs_per_round)#.batch(
-            #FLAGS.batch_size, drop_remainder=False)
+    def preprocess_train_dataset(dataset):
+        # Use buffer_size same as the maximum client dataset size,
+        # 418 for Federated EMNIST
+        return dataset.map(element_fn).shuffle(buffer_size=418).repeat(
+            count=FLAGS.client_epochs_per_round)
 
-  def preprocess_test_dataset(dataset):
-    return dataset.map(element_fn).batch(
-        FLAGS.test_batch_size, drop_remainder=False)
+    def preprocess_test_dataset(dataset):
+        return dataset.map(element_fn).batch(
+            FLAGS.test_batch_size, drop_remainder=False)
 
-  emnist_train = emnist_train.preprocess(preprocess_train_dataset)
-  emnist_test = preprocess_test_dataset(
-      emnist_test.create_tf_dataset_from_all_clients())
-  return emnist_train, emnist_test
+    emnist_train = emnist_train.preprocess(preprocess_train_dataset)
+    emnist_test = preprocess_test_dataset(
+        emnist_test.create_tf_dataset_from_all_clients())
+    return emnist_train, emnist_test
 
 
 def create_original_fedavg_cnn_model(only_digits=False):
-  """The CNN model used in https://arxiv.org/abs/1602.05629.
+    """The CNN model used in https://arxiv.org/abs/1602.05629.
 
   Args:
     only_digits: If True, uses a final layer with 10 outputs, for use with the
@@ -79,127 +79,119 @@ def create_original_fedavg_cnn_model(only_digits=False):
   Returns:
     An uncompiled `tf.keras.Model`.
   """
-  data_format = 'channels_last'
-  input_shape = [28, 28, 1]
+    data_format = 'channels_last'
+    input_shape = [28, 28, 1]
 
-  max_pool = functools.partial(
-      tf.keras.layers.MaxPooling2D,
-      pool_size=(2, 2),
-      padding='same',
-      data_format=data_format)
-  conv2d = functools.partial(
-      tf.keras.layers.Conv2D,
-      kernel_size=5,
-      padding='same',
-      data_format=data_format,
-      activation=tf.nn.relu)
+    max_pool = functools.partial(
+        tf.keras.layers.MaxPooling2D,
+        pool_size=(2, 2),
+        padding='same',
+        data_format=data_format)
+    conv2d = functools.partial(
+        tf.keras.layers.Conv2D,
+        kernel_size=5,
+        padding='same',
+        data_format=data_format,
+        activation=tf.nn.relu)
 
-  model = tf.keras.models.Sequential([
-      conv2d(filters=32, input_shape=input_shape),
-      max_pool(),
-      conv2d(filters=64),
-      max_pool(),
-      tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(512, activation=tf.nn.relu),
-      tf.keras.layers.Dense(10 if only_digits else 62),
-  ])
+    model = tf.keras.models.Sequential([
+        conv2d(filters=32, input_shape=input_shape),
+        max_pool(),
+        conv2d(filters=64),
+        max_pool(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10 if only_digits else 62),
+    ])
 
-  return model
+    return model
 
 
 def server_optimizer_fn():
-  return tf.keras.optimizers.SGD(learning_rate=FLAGS.server_learning_rate)
+    return tf.keras.optimizers.SGD(learning_rate=FLAGS.server_learning_rate)
 
 
 def client_optimizer_fn():
-  return tf.keras.optimizers.SGD(learning_rate=FLAGS.client_learning_rate)
+    return tf.keras.optimizers.SGD(learning_rate=FLAGS.client_learning_rate)
 
-
-"""
-def server_optimizer_fn():
-  return tf.keras.optimizers.SGD(learning_rate=FLAGS.server_learning_rate, momentum=0.9, nesterov=True)
-
-
-def client_optimizer_fn():
-  return tf.keras.optimizers.SGD(learning_rate=FLAGS.client_learning_rate, momentum=0.9, nesterov=True)
-"""
 
 def main(argv):
-  if len(argv) > 1:
-    raise app.UsageError('Too many command-line arguments.')
+    if len(argv) > 1:
+        raise app.UsageError('Too many command-line arguments.')
 
-  # Preparing the directory to save run results
-  root_logdir = os.path.abspath(os.path.join("logs", "SSTC"))
+    # Preparing the directory to save run results
+    # This may throw errors if there is no logs/SSTC dir existing
+    root_logdir = os.path.abspath(os.path.join("logs", "SSTC"))
 
-  run_id = "C{0}_E{1}_SSTC".format(str(FLAGS.train_clients_per_round),
-                                      str(FLAGS.client_epochs_per_round))
-  run_logdir_test = os.path.join(root_logdir, run_id, "test")
-  test_summary_writer = tf.summary.create_file_writer(run_logdir_test)
+    run_id = "C{0}_E{1}_SSTC".format(str(FLAGS.train_clients_per_round),
+                                     str(FLAGS.client_epochs_per_round))
+    run_logdir_test = os.path.join(root_logdir, run_id, "test")
+    test_summary_writer = tf.summary.create_file_writer(run_logdir_test)
 
-  # If GPU is provided, TFF will by default use the first GPU like TF. The
-  # following lines will configure TFF to use multi-GPUs and distribute client
-  # computation on the GPUs. Note that we put server computatoin on CPU to avoid
-  # potential out of memory issue when a large number of clients is sampled per
-  # round. The client devices below can be an empty list when no GPU could be
-  # detected by TF.
-  client_devices = tf.config.list_logical_devices('GPU')
-  server_device = tf.config.list_logical_devices('CPU')[0]
-  tff.backends.native.set_local_execution_context(
-      server_tf_device=server_device, client_tf_devices=client_devices)
+    # If GPU is provided, TFF will by default use the first GPU like TF. The
+    # following lines will configure TFF to use multi-GPUs and distribute client
+    # computation on the GPUs. Note that we put server computatoin on CPU to avoid
+    # potential out of memory issue when a large number of clients is sampled per
+    # round. The client devices below can be an empty list when no GPU could be
+    # detected by TF.
+    client_devices = tf.config.list_logical_devices('GPU')
+    server_device = tf.config.list_logical_devices('CPU')[0]
+    tff.backends.native.set_local_execution_context(
+        server_tf_device=server_device, client_tf_devices=client_devices)
 
-  # Load the dataset
-  train_data, test_data = get_emnist_dataset()
+    # Load the dataset
+    train_data, test_data = get_emnist_dataset()
 
-  def tff_model_fn():
-    """Constructs a fully initialized model for use in federated averaging."""
-    keras_model = create_original_fedavg_cnn_model(only_digits=False)
-    keras_model.summary()
-    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    return simple_fedavg_tf.KerasModelWrapper(keras_model,
-                                              test_data.element_spec, loss)
+    def tff_model_fn():
+        """Constructs a fully initialized model for use in federated averaging."""
+        keras_model = create_original_fedavg_cnn_model(only_digits=False)
+        keras_model.summary()
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        return simple_fedavg_tf.KerasModelWrapper(keras_model,
+                                                  test_data.element_spec, loss)
 
-  iterative_process = simple_fedavg_tff.build_federated_averaging_process(
-      tff_model_fn, server_optimizer_fn, client_optimizer_fn)
-  server_state = iterative_process.initialize()
+    iterative_process = simple_fedavg_tff.build_federated_averaging_process(
+        tff_model_fn, server_optimizer_fn, client_optimizer_fn)
+    server_state = iterative_process.initialize()
 
-  metric = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
-  model = tff_model_fn()
+    metric = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+    model = tff_model_fn()
 
-  seed = 0
-  for round_num in range(FLAGS.total_rounds):
-    # For fair comparison among algorithms, we select the same pool of per-round participants using a seed
-    np.random.seed(seed)
-    seed = seed + 1
-    sampled_clients = np.random.choice(
-        train_data.client_ids,
-        size=FLAGS.train_clients_per_round,
-        #size=1,
-        replace=False)
-    sampled_train_data = [
-        train_data.create_tf_dataset_for_client(client).batch(
-            FLAGS.batch_size, drop_remainder=False)
-        for client in sampled_clients
-    ]
+    seed = 0
+    for round_num in range(FLAGS.total_rounds):
+        # For fair comparison among algorithms, we select the same pool of per-round participants using a seed
+        np.random.seed(seed)
+        seed = seed + 1
+        sampled_clients = np.random.choice(
+            train_data.client_ids,
+            size=FLAGS.train_clients_per_round,
+            replace=False)
+        sampled_train_data = [
+            train_data.create_tf_dataset_for_client(client).batch(
+                FLAGS.batch_size, drop_remainder=False)
+            for client in sampled_clients
+        ]
 
-    # Run the next round of the training
-    server_state, train_metrics, time_metrics = iterative_process.next(
-        server_state, sampled_train_data)
+        # Run the next round of the training
+        server_state, train_metrics, time_metrics = iterative_process.next(
+            server_state, sampled_train_data)
 
-    print(f'Round {round_num} training loss: {train_metrics} mean time: {time_metrics}')
-    print(" Local Training Average Time {:.7f}".format(time_metrics))
+        print(f'Round {round_num} training loss: {train_metrics} mean time: {time_metrics}')
+        print(" Local Training Average Time {:.7f}".format(time_metrics))
 
-    # Evaluating the global model on the test set
-    if round_num % FLAGS.rounds_per_eval == 0:
-      model.from_weights(server_state.model_weights)
-      accuracy = simple_fedavg_tf.keras_evaluate(model.keras_model, test_data,
-                                                 metric)
-      print(f'Round {round_num} validation accuracy: {accuracy * 100.0}')
+        # Evaluating the global model on the test set
+        if round_num % FLAGS.rounds_per_eval == 0:
+            model.from_weights(server_state.model_weights)
+            accuracy = simple_fedavg_tf.keras_evaluate(model.keras_model, test_data,
+                                                       metric)
+            print(f'Round {round_num} validation accuracy: {accuracy * 100.0}')
 
-    # Writing the results on disk
-    # To be then visualized or post-processed
-    with test_summary_writer.as_default():
-        tf.summary.scalar("sparse_categorical_accuracy", accuracy, step=round_num)
-        test_summary_writer.flush()
+        # Writing the results on disk
+        # To be then visualized or post-processed
+        with test_summary_writer.as_default():
+            tf.summary.scalar("sparse_categorical_accuracy", accuracy, step=round_num)
+            test_summary_writer.flush()
+
 
 if __name__ == '__main__':
-  app.run(main)
+    app.run(main)
